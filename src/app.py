51837ea492bbedaa3026cf75042a2406e0825e50
César Jovanni Zaldivar Camps <132796806+Cesarzaldivar@users.noreply.github.com>
2024-07-19 16:39:45 +0000

    family api

--- src/app.py
+++ src/app.py
@@ -15,6 +15,24 @@ CORS(app)
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+jackson_family.add_member({
+                "first_name": "John",
+                "last_name": "Jackson",
+                "age": 33,
+                "lucky_numbers": [7,13,22]
+            })
+jackson_family.add_member(   {
+                "first_name": "Jane",
+                "last_name": "Jackson",
+                "age": 35,
+                "lucky_numbers": [10,14,3]
+            })
+jackson_family.add_member({
+                "first_name": "Jimmy",
+                "last_name": "Jackson",
+                "age": 5,
+                "lucky_numbers": [1]
+            })
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -25,18 +43,55 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/members', methods=['GET'])
-def handle_hello():
+
+@app.route('/member', methods=['POST'])
+def create_one_member():
 
     # this is how you can use the Family datastructure by calling its methods
+    body = request.get_json()
+    if body is None:
+        return jsonify({"error": "not members"}), 400
+    id = body.get("id",None)
+    first_name = body.get("first_name",None)
+    age = body.get("age",None)
+    lucky_numbers = body.get("lucky_numbers",None)
+
+    if first_name is None:
+        return jsonify({"error": "first name is required"}), 400
+    
+    if age is None:
+        return jsonify({"error": "age is required"}), 400
+    
+    if lucky_numbers is None:
+        return jsonify({"error": "lucky numbers is required"}), 400
+    
+    jackson_family.add_member(body)
+    return "memeber created",200
+    
+
+@app.route('/members', methods=['GET'])
+def get_all_members():
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    if members is not None:
+        return jsonify(members),200
+    else:
+        return jsonify({"Not members"}),400
+    
+
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    member_family = jackson_family.get_member(member_id)
+    if not member_family:
+        return jsonify({"member not exist"}),400
+    return jsonify(member_family),200
 
 
-    return jsonify(response_body), 200
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_family_member(member_id):
+    delete_member_family = jackson_family.delete_member(member_id)
+    if not delete_member_family:
+        return jsonify({"msg":"Familiar no encontrado"}), 400
+    return jsonify({"done": delete_member_family}) , 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
